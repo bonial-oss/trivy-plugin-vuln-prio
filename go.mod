3e670b5abd8a94fad3b9ca9a8c5fb94b4727d7d8
Patrick Jungermann <Patrick.Jungermann@gmail.com>
2026-02-12 21:41:29 +0100

    chore: scaffold project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bonial-oss/trivy-plugin-vuln-prio
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2026 Bonial International GmbH
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Fprintln(os.Stderr, "vuln-prio: not yet implemented")
+	os.Exit(2)
+}
