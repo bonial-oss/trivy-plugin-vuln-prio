f625990d5749b40478020180a0c8e8f61b3abc4c
Patrick Jungermann <Patrick.Jungermann@gmail.com>
2026-02-13 01:02:47 +0100

    feat(cli): add cobra CLI and wire up full enrichment pipeline

--- main.go
+++ main.go
@@ -4,11 +4,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+
+	"github.com/bonial-oss/trivy-plugin-vuln-prio/cmd"
 )
 
 func main() {
-	fmt.Fprintln(os.Stderr, "vuln-prio: not yet implemented")
-	os.Exit(2)
+	if err := cmd.NewRootCommand().Execute(); err != nil {
+		var exitErr *cmd.ExitError
+		if errors.As(err, &exitErr) {
+			if exitErr.Message != "" {
+				fmt.Fprintf(os.Stderr, "Error: %s\n", exitErr.Message)
+			}
+			os.Exit(exitErr.Code)
+		}
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(2)
+	}
 }
